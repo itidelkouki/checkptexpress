d310483bf4a538b8b8e8a807da1a3d759d9803e2
ASUS <aatidelkouki@gmail.com>
2021-06-16 13:43:37 +0100

    important

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Switch from 'react-bootstrap/esm/Switch';
+import Homepage from './components/Homepage';
+import { Route } from "react-router-dom";
+import PatientArea from './components/PatientArea';
+import NavMenu from './components/NavMenu';
+import DoctorArea from './components/DoctorArea';
+import OurServices from './components/OurServices';
+function App() {
+  return (
+    <div className="App">
+  
+  <Switch>
+  <NavMenu/>
+  
+         <Route exact path="/" component={Homepage} />
+         <Route path="/patient" component={PatientArea} />
+         <Route path="/doctors" component={DoctorArea} />
+         <Route path="/services" component={OurServices} />
+
+  </Switch>
+          
+      
+    </div>
+  );
+}
+
+export default App;
+//<Route path="/login" component={Login} />
+//<Route path="/explore" component={Explore} />
---
+++ client/src/components/OurServices.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './OurServices.css';
+
+
+const OurServices= () =>{
+    return(
+
+      
+     <div className="container">
+     <div class="contact-container">
+<h1>
+    OurServices
+</h1>
+
+     </div>
+     </div>)};
+
+     export default OurServices;
